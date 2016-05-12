1496469a4d97acf6002496f182398c24877bd0fa
Hootas <Hootas@outlook.com>
2016-05-12 18:26:51 +0300

    Add files via upload

---
+++ Unit1.h
@@ -0,0 +1,41 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Imaging.jpeg.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TImage *img1;
+	TImage *img2;
+	TImage *img3;
+	TImage *img4;
+	TImage *img5;
+	TImage *img6;
+	TImage *img7;
+	TImage *img8;
+	TImage *img9;
+	TImage *img11;
+	TImage *img12;
+	TImage *img13;
+	TImage *img17;
+	TButton *btn1;
+	TLabel *lbl1;
+	TLabel *lbl2;
+	TLabel *lbl3;
+	TButton *btn2;
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
